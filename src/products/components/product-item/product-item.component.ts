2ed2083513f8055d19e5a3f1fdb947f8af4b1a5b
Sergio Kagiema <sergio_kagiema@live.com>
2022-08-01 21:28:24 +0300

    Refactor product item implementation

--- src/products/components/product-item/product-item.component.ts
+++ src/products/components/product-item/product-item.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/services/shopping-store.service';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.scss']
+  styleUrls: ['./product-item.component.scss'],
 })
 export class ProductItemComponent implements OnInit {
+  @Input() public product: Product | null = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,13 +1,14 @@
 // Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ProductItemComponent } from './components/product-item/product-item.component';
 
 // Components
 import { ProductListComponent } from './components/products-list/products-list.component';
 
 @NgModule({
-  declarations: [ProductListComponent],
+  declarations: [ProductListComponent, ProductItemComponent],
   imports: [BrowserModule],
-  exports: [ProductListComponent],
+  exports: [ProductListComponent, ProductItemComponent],
 })
 export class ProductsModule {}
